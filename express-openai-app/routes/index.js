cbf4508e0d8e8d5ab317f4d1bdfc8a9b0ac339f1
yuji141 <orexcalibur@gmail.com>
2025-07-06 14:08:03 +0900

    index.js追記

--- express-openai-app/routes/index.js
+++ express-openai-app/routes/index.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+const { Configuration, OpenAIApi } = require('openai');
+const api_key = "";
+const config = new Configuration({
+  apiKey: api_key,
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
